bd7b03e0a46264138f991c42d8485764e2b3990b
Eitan <evbambly@gmail.com>
2021-02-24 11:06:54 +0200

    minor changes

--- packages/donor/src/utils/validations/donor.ts
+++ packages/donor/src/utils/validations/donor.ts
@@ -1,7 +1,9 @@
+import {Donor} from "@zm-blood-components/common"
 import vest, { test, enforce } from "vest";
+import { IVestResult } from "vest/vestResult";
 
-export default vest.create((data = {}) => {
+export  default vest.create((data: Donor) => {
   test("username", "Username is required", () => {
     enforce(data.username).isNotEmpty();
   });
-});
+}) as (donor: Donor) => keyof Donor;
