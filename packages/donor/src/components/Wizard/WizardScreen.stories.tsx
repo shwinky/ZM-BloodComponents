8607e1e440dc24c6544140f78dbf23d701424ebe
Moris R <moris.rafol@gmail.com>
2021-04-21 01:05:42 +0300

    donor: update storybook

--- packages/donor/src/components/Wizard/WizardScreen.stories.tsx
+++ packages/donor/src/components/Wizard/WizardScreen.stories.tsx
@@ -1,7 +1,6 @@
 import WizardScreen, { WizardScreenProps } from "./WizardScreen";
 import { ButtonVariant } from "../basic/Button";
 import { MuiTestWrapper } from "../../__test__/TestUtils";
-import Div100vh from "react-div-100vh";
 
 export default {
   component: WizardScreen,
@@ -10,46 +9,32 @@ export default {
 
 const baseArgs: WizardScreenProps = {
   pages: [
-    "אפליקציה זו מיועדת עבור תורמי הטרומבוציטים של זכרון מנחם המגיעים העוזרים להציל חיים של ילדים חולי סרטן, וחולים במחלות קשות נוספות!",
-    "אפליקציה זו מיועדת עבור תורמי הטרומבוציטים של זכרון מנחם המגיעים העוזרים להציל חיים של ילדים חולי סרטן, וחולים במחלות קשות נוספות!",
+    {
+      content:
+        "אפליקציה זו מיועדת עבור תורמי הטרומבוציטים של זכרון מנחם המגיעים העוזרים להציל חיים של ילדים חולי סרטן, וחולים במחלות קשות נוספות!",
+      backgroundColor: "antiquewhite",
+    },
+    {
+      content:
+        "אפליקציה זו מיועדת עבור תורמי הטרומבוציטים של זכרון מנחם המגיעים העוזרים להציל חיים של ילדים חולי סרטן, וחולים במחלות קשות נוספות!",
+      backgroundColor: "lightblue",
+    },
     "אפליקציה זו מיועדת עבור תורמי הטרומבוציטים של זכרון מנחם המגיעים העוזרים להציל חיים של ילדים חולי סרטן, וחולים במחלות קשות נוספות!",
   ],
-
   buttonText: "אישור והמשך",
   buttonVariant: ButtonVariant.outlined,
+  completeText: "סיים",
+  completeVariant: ButtonVariant.contained,
+  showStepperButtons: true,
+  onButtonClick: "next",
 };
-export const FirstStep = (args: WizardScreenProps) => (
+export const Wizard = (args: WizardScreenProps) => (
   <MuiTestWrapper>
-    <Div100vh>
+    <div style={{ height: "500px", width: "300px", border: "2px black solid" }}>
       <WizardScreen {...args} />
-    </Div100vh>
+    </div>
   </MuiTestWrapper>
 );
-FirstStep.args = {
+Wizard.args = {
   ...baseArgs,
-  currentStep: 0,
-  onPrev: undefined,
-};
-
-export const MiddleStep = (args: WizardScreenProps) => (
-  <MuiTestWrapper>
-    <Div100vh>
-      <WizardScreen {...args} />
-    </Div100vh>
-  </MuiTestWrapper>
-);
-MiddleStep.args = baseArgs;
-
-export const LastStep = (args: WizardScreenProps) => (
-  <MuiTestWrapper>
-    <Div100vh>
-      <WizardScreen {...args} />
-    </Div100vh>
-  </MuiTestWrapper>
-);
-LastStep.args = {
-  ...baseArgs,
-  currentStep: 3,
-  onNext: undefined,
-  buttonVariant: ButtonVariant.contained,
 };
